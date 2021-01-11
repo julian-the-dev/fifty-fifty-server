18e3fdd2973ad311df837228f7142caa88b6abd7
Your Name <you@example.com>
2021-01-11 13:24:07 +0100

    start login process

--- authServer.ts
+++ authServer.ts
@@ -1,16 +1,18 @@
-import { DbUtils } from './utils/db.utils';
+import { COMMON } from "./utils/common.const";
+import { DbUtils } from "./utils/db.utils";
 import cookieParser from "cookie-parser";
 import express from "express";
 import createError from "http-errors";
 import logger from "morgan";
 import path from "path";
 import dotenv from "dotenv";
+import bcrypt from "bcrypt";
 
 async function main() {
   // initialize configuration
   dotenv.config();
   const app = init();
-  DbUtils.connect(process.env.DATABASE)
+  DbUtils.connect(process.env.DATABASE);
   const port = process.env.AUTH_PORT;
   initRoutes(app);
   handleErrors(app);
@@ -29,15 +31,46 @@ function init() {
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
   app.use(express.static(path.join(__dirname, "public")));
-  app.get('/', (req,res) => res.send('Express + TypeScript Server'));
+  app.get("/", (req, res) => res.send("Express + TypeScript Server"));
   return app;
 }
 
 function initRoutes(app) {
-  app.get('/signup', (req,res) => res.send('Express + TypeScript Server'));
-  app.get('/login', (req,res) => res.send('Express + TypeScript Server'));
-  app.get('/token', (req,res) => res.send('Express + TypeScript Server'));
-  app.get('/logout', (req,res) => res.send('Express + TypeScript Server'));
+  app.get("/signup", async (req, res) => {
+    const password = req.body.password;
+    const email = req.body.email;
+    const username = req.body.username;
+    const collUsers = DbUtils.getDB().collection("users");
+    const userFound = await collUsers
+      .find({ $or: [{ username }, { email }] })
+      .toArray();
+    if (userFound) {
+      res.status(500);
+      res.send("Error user found");
+    } else {
+      const hash = await bcrypt.hash(password, COMMON.saltRounds);
+      collUsers.insertOne({ username, email, hash });
+    }
+  });
+  app.get("/login", async (req, res) => {
+    const password = req.body.password;
+    const username = req.body.username;
+    const collUsers = DbUtils.getDB().collection("users");
+    const userFound = await collUsers.findOne({ username });
+    if (userFound) {
+      const passwordMatch = await bcrypt.compare(password, userFound.hash);
+      if (passwordMatch) {
+      } else {
+        res.status(503);
+        res.send("Password does not match!");
+      }
+    } else {
+      res.status(500);
+      res.send("Error user not found");
+    }
+  });
+  app.get("/token", (req, res) => res.send("Express + TypeScript Server"));
+  app.get("/logout", (req, res) => res.send("Express + TypeScript Server"));
 }
 
 function handleErrors(app) {
